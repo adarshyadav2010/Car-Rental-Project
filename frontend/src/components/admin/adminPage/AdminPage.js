7b90b46f3f0926bef4eda083df2bb29e2b0a0143
Marella <neeharikamarella3@gmail.com>
2023-06-12 19:28:11 -0700

    Added tokens and item details

--- frontend/src/components/admin/adminPage/AdminPage.js
+++ frontend/src/components/admin/adminPage/AdminPage.js
@@ -1,17 +1,38 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import AdminNav from "../../navbar/AdminsNav";
 import CarCards from "./CarCards";
 import '../../styles/styles.css'
-import {StateContextsData} from '../../context/StateContext'
+import { StateContextsData } from '../../context/StateContext'
+import { GetCars } from "../../utils/ApiUtils";
 
-export default function AdminPage(){
-    const {cars, setCars} = useContext(StateContextsData)
-    return(
+export default function AdminPage() {
+    const AdminId = JSON.parse(localStorage.getItem("Admin-Id"));
+    const navigate = useNavigate();
+    const TokenAdmin = JSON.parse(localStorage.getItem("token-admin"))
+    const [err, setErr] = useState("");
+    const [ok, setOk] = useState("");
+    const { car, setCar, setEdit } = useContext(StateContextsData);
+    useEffect(() => {
+        if (!localStorage.getItem("token-admin")) {
+            return navigate("/")
+        }
+        GetCars()
+            .then(data => setCar(data.reverse()))
+    }, []
+    );
+
+    const adminName = JSON.parse(localStorage.getItem("Admin-name"))
+
+    console.log(adminName);
+
+    console.log(car)
+
+    return (
         <div className='admin-container'>
-            <AdminNav/>
+            <AdminNav />
             <div className="admin-page">
                 <h1 className="heading-admin">Hello Admin...</h1>
                 <div>
@@ -19,28 +40,60 @@ export default function AdminPage(){
                         <h3>Cars</h3>
                         <Link to='/add-car-details'><button className="save-details">Add Cars</button></Link>
                     </div>
-                    
+                    {car.length === 0 ? <Backdrop
+                        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                        open>
+                        <CircularProgress color="inherit" />
+                    </Backdrop> : <>
+                        {err && <div id="display-message-on-the-admin-pag-for-the-err">{err} <span
+                            onClick={() => {
+                                setErr("");
+                                setOk("")
+                            }} id="button-ok-for-th-delee-things">{ok}</span></div>}
 
-                    <div className="main-container">
-                        {/* {
-                            cars.map((data, i)=>{
-                                return(
-                                    <CarCards car={data}/>
-                                )
-                            })
-                            
-                        } */}
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                        <CarCards/>
-                    </div>
-                    
+                        <div className="main-container">
+                            {
+                                car.map((data, i) => {
+                                    return (
+                                        <div key={i} className="car-container" onClick={() => navigate("/edit-car-page")}>
+                                            <div className="img-container">
+                                                <img src={``} id="car-img" alt="img-car"
+                                                    onClick={() => {
+                                                        // if (AdminId !== d.AdminId) {
+                                                        //     console.log(d.AdminId)
+                                                        //     setErr("You Don`t Have Access To Edit This Details");
+                                                        //     setOk("OK")
+                                                        // }
+                                                        // else {
+                                                        //     console.log(d.AdminId)
+                                                        //     setEdit(d);
+                                                        //     Navigater("/edit-car-details")
+                                                        // }
+
+                                                    }} />
+                                            </div>
+                                            <div className="card-2nd">
+                                                <div className="person-nos">6 person</div>
+                                                <div className="car-details">
+                                                    <span>name</span>
+                                                    <span className="distance-det">per km KM/RS</span>
+                                                </div>
+                                            </div>
+
+                                            <hr />
+                                            <div className="date-details">
+                                                <span >Available Date</span>
+                                                <span >start - till</span>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+
+                            }
+
+                        </div>
+                    </>
+                    }
                 </div>
             </div>
 
